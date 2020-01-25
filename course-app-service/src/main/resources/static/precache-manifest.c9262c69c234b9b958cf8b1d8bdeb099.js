e4c21d9ec4d6716bec89519ccc6110c9b3f4cec6
Swastik Panda <swastiksuman@gmail.com>
2020-01-25 22:16:31 +0530

    dockerworking

---
+++ course-app-service/src/main/resources/static/precache-manifest.c9262c69c234b9b958cf8b1d8bdeb099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115503c9a7d8e08c8c46c358d94f1683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2bb652576f45b505511",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7b332fd9312147108fa8",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a2bb652576f45b505511",
+    "url": "/static/js/2.7919765a.chunk.js"
+  },
+  {
+    "revision": "7b332fd9312147108fa8",
+    "url": "/static/js/main.491c0aac.chunk.js"
+  },
+  {
+    "revision": "edddd6e72f140f530ffb",
+    "url": "/static/js/runtime-main.001dbed8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
