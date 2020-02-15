cffa0557cb9503ff83ce1224cbbafa40942b7b70
Swastik Panda <swastiksuman@gmail.com>
2020-02-15 23:40:25 +0530

    added logs

--- course-app-service/src/main/java/com/swastik/courseappservice/service/SnippetService.java
+++ course-app-service/src/main/java/com/swastik/courseappservice/service/SnippetService.java
@@ -3,6 +3,8 @@ package com.swastik.courseappservice.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +17,12 @@ public class SnippetService {
 	@Autowired
 	SnippetRepository snippetRepository;
 	
+	Logger logger = LoggerFactory.getLogger(SnippetService.class);
+	
 	public List<Snippet> getAllSnippets(){
 		List<Snippet> listOfSnippets = new ArrayList<Snippet>();
 		snippetRepository.findAll().forEach((data) -> {
+			logger.debug(data.toString());
 			listOfSnippets.add(data);
 		});
 		return listOfSnippets;
